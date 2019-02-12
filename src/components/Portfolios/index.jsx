e0181db9f53f4261b98b456ea8dac2bfca349e16
Alexandra <alexswartz9753@gmail.com>
2019-02-12 11:12:29 -0500

    added profile image

--- src/components/Portfolios/index.jsx
+++ src/components/Portfolios/index.jsx
@@ -4,13 +4,24 @@ import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 import { isEmpty, isArray, includes, map, get, first } from 'lodash/fp';
 import SimpleWrapper from '~/components/Common/SimpleWrapper';
+import * as image from '../../images/image.jpg';
 
 const Wrapper = SimpleWrapper.extend`
   padding: 100px 0 0;
+  width: 70%;
+  margin: 0 auto;
   @media (max-width: 414px) {
     padding: 70px 0 0;
   }
+
+  img {
+    border-radius: 100%;
+    height: 150px;
+    width: 150px;
+  }
 `;
+
+
 
 const Portfolios = ({ data }) => {
   const portfolios = get('allMarkdownRemark.edges')(data);
@@ -23,7 +34,7 @@ const Portfolios = ({ data }) => {
         </title>
         <meta name="og:title" content="ALEX | ABOUT" />
       </Helmet>
-     
+     <img src={image}></img>
     </Wrapper>
   );
 };
